db1799280e6a09dd440c7cd4da4575ec9350c434
M Atheer <55885230+masreplay@users.noreply.github.com>
2022-09-14 02:26:58 +0300

    add: flutter native splash

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,17 @@ import 'package:aswar/app.dart';
 import 'package:aswar/common_libs.dart';
 import 'package:aswar/di/injection.dart';
 import 'package:aswar/ui/strings.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 Future<void> main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   configureInjection();
   runApp(const AswarApp());
   await bootstrap();
+
+  FlutterNativeSplash.remove();
 }
 
 Future<void> bootstrap() async {
